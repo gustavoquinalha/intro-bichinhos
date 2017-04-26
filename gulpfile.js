e8a9a1e55bc5369936d586c6a47bf1bc0a5e4595
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-04-26 15:04:08 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-var gulp = require('gulp'),
-  imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var uncss = require('gulp-uncss');
 
 gulp.task('build-img', function(){
     gulp.src('img/**/*')
                    .pipe(imagemin())
                    .pipe(gulp.dest('img'));
 })
+
+gulp.task('default', function () {
+    return gulp.src('css/bichinhos.css')
+        .pipe(uncss({
+            html: ['*.html']
+        }))
+        .pipe(gulp.dest('css/out'));
+});
